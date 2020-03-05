dfdf1a20c7a1b584725b76759d5e219588e232dc
Thamonwan Maneechan <thamonwan.top@gmail.com>
2020-03-05 19:17:28 +0700

    finished IB PHP OOP project

---
+++ 04-OOP-Concepts/tests/AbstractTest.php
@@ -0,0 +1,6 @@
+
+<?php
+
+require '../includes/init.php';
+
+$emp = new Employee('Alex', 25000, 5000);
---
+++ 04-OOP-Concepts/tests/ManagerTest.php
@@ -0,0 +1,7 @@
+  
+<?php
+
+require '../includes/init.php';
+
+$obj = new Manager('Somporn', 50000, 2000, 5000);
+$obj->display();
